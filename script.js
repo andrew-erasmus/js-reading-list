ff8951622a400b856c814ed0eedf48870e850318
Andrew Erasmus <andrew.r.eras@gmail.com>
2024-07-05 16:52:19 +0200

    Add form collection for book information

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const readingList = [];
 const addBookDialog = document.getElementById('addBookDialog');
+
+
 let exitButton = document.getElementById('closeAddModal');
+let titleInput = document.getElementById('titleInput');
+let authorInput = document.getElementById('authorInput');
+let lengthInput = document.getElementById('lengthInput');
+let yesRadio = document.getElementById('yesRadio');
+let noRadio = document.getElementById('noRadio');
+let submitBookButton = document.getElementById('submitBook');
+
 // Constructor for a new book
 function Book(title, author, pages, read){
     this.title = title;
@@ -21,11 +30,23 @@ function displayAddBookModal(){
     });
 }
 
+submitBookButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let length = lengthInput.value;
+    let read = false;
+    if (yesRadio.checked) {
+        read = true;
+    }
+
+    addBookToList(title, author, length, read);
+});
+
 function addBookToList(title, author, pages, read){
     let bookToAdd = new Book (title, author, pages, read);
     readingList.push(bookToAdd);
-    addBookDialog.showModal();
-
+    console.log(bookToAdd.info());
 }
 
 function displayBooks(){
